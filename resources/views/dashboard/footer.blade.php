d1f7388a8f8038563b6ed46e5e20d7c2072a4674
Yonathan Castillo <leothan522@gmail.com>
2024-08-21 20:21:10 -0400

    Composer update | Actualizado Footer

--- resources/views/dashboard/footer.blade.php
+++ resources/views/dashboard/footer.blade.php
@@ -1,6 +1,6 @@
 <div class="float-right d-none d-sm-block">
     <b>Version</b> 1.0
 </div>
-<strong>&copy; 2024 {{ config('app.name') }}
+<span>&copy; 2024 {{ config('app.name') }}
     | Dv. <a href="https://t.me/Leothan" target="_blank">Ing. Yonathan Castillo</a>
-</strong>
+</span>
