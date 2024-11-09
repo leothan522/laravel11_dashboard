a58c378a67cace78008bbfcd4a86fab8a809bc2c
Yonathan Castillo <leothan522@gmail.com>
2024-11-09 08:57:27 -0400

    Composer update

--- app/Http/Funciones/Funciones.php
+++ app/Http/Funciones/Funciones.php
@@ -6,9 +6,12 @@ use Carbon\Carbon;
 use Illuminate\Support\Facades\Storage;
 use Intervention\Image\ImageManager;
 
-function generarStringAleatorio($largo = 10, $espacio = false): string
+function generarStringAleatorio($largo = 10, $soloNumeros = false, $espacio = false): string
 {
     $caracteres = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    if ($soloNumeros){
+        $caracteres = '0123456789';
+    }
     $caracteres = $espacio ? $caracteres . ' ' : $caracteres;
     $string = '';
     for ($i = 0; $i < $largo; $i++) {
@@ -87,7 +90,7 @@ function numRowsPaginate(): int
     $num = 15;
     $parametro = Parametro::where("nombre", "numRowsPaginate")->first();
     if ($parametro) {
-        if (is_int($parametro->valor)) {
+        if (intval($parametro->valor)) {
             $num = intval($parametro->valor);
         }
     }
